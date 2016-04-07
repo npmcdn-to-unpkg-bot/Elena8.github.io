ed5043d163d0144f9ce33c146221b15933b36617
Elena8 <mol4anova_@mail.ru>
2016-04-08 00:29:13 +0300

    create addname

--- js_module_1(2)/js/script.js
+++ js_module_1(2)/js/script.js
@@ -1,14 +1,10 @@
 var name1=[];
-function promptAdd(){
-    for(var i=0; i<5; i++){
+function addName(nameUser){
+var flag;
+for(var i=0; i<5; i++){
 	var namePrompt=prompt('Enter name :');
 	name1.push(namePrompt);
     }
-}
-
-function addName(nameUser){
-prompt();	
-var flag;
 for(var j=0; j<=name1.length-1; j++){
 	if(name1[j]==nameUser && (name1[j]!==null) && (name1[j]!==''))
 	{
@@ -27,7 +23,7 @@ for(var j=0; j<=name1.length-1; j++){
         }else{
 	alert('ERROR!!');
         }
-}
 
+}
 var nameUser=prompt("Enter YOUR name");
 addName(nameUser);
