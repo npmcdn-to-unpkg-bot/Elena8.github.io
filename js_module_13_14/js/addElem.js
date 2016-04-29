c08a4a55cd686c8ff0468b1326b100f5b2d3a18c
Elena Danilenko <mol4anova_@mail.ru>
2016-04-30 00:08:48 +0300

    add js 13-14

---
+++ js_module_13_14/js/addElem.js
@@ -0,0 +1,101 @@
+var obj = {
+
+        createElements: function (varName, tag, nameClass, mainTagVar, mainTag, position) {
+            varName = document.createElement(tag);
+            varName.classList.add(nameClass);
+            mainTagVar = document.querySelector(mainTag);
+            mainTagVar.insertBefore(varName, mainTagVar.children[position]);
+        },
+
+        createLabels: function (tag, classEl, parentsEl) {
+            tag = document.createElement(tag);
+            parentsEl.appendChild(tag);
+            tag.classList.add(classEl);
+        },
+
+        createInputs: function (tag, classEl, parentsEl, type, position) {
+            tag = document.createElement(tag);
+            parentsEl.insertBefore(tag, parentsEl.children[position]);
+            tag.setAttribute('type', type);
+            tag.classList.add(classEl);
+        },
+
+        createText: function (varTag, text, position) {
+	        varTag.innerHTML = '<span>' + text + '</span>';
+        }
+
+    };
+var answerArr = ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'];
+var tagTag;
+var classLi1;
+var classLi2;
+var classLi3;
+var elLabel;
+obj.createElements('wrap', 'div', 'wrapper', 'mainWrap', 'body', 0);
+obj.createElements('hh1', 'h1', 'hh_1', 'wrap', '.wrapper', 0);
+tagTag = document.querySelector('h1');
+obj.createText(tagTag, 'Тест по программированию');
+obj.createElements('mainForm', 'form', 'formMain', 'wrap', '.wrapper', 1);
+obj.createElements('oll', 'ol', 'list', 'mainForm', 'form', 0);
+//Create li
+var i;
+for (i = 0; i < 3; i + 1) {
+    obj.createElements('li' + [i + 1], 'li', 'list-item' + [i + 1], 'ol1',  'ol', i);
+}
+
+classLi1 = document.getElementsByClassName('list-item1');
+classLi2 = document.getElementsByClassName('list-item2');
+classLi3 = document.getElementsByClassName('list-item3');
+
+obj.createText(classLi1[0], 'Вопрос №1', 0);
+obj.createLabels('br', 'br', classLi1[0]);
+obj.createText(classLi2[0], 'Вопрос №2', 0);
+obj.createLabels('br', 'br', classLi2[0]);
+obj.createText(classLi3[0], 'Вопрос №3', 0);
+obj.createLabels('br', 'br', classLi3[0]);
+//create labels
+var j;
+for (j = 0; j < 3; j + 1) {
+    obj.createLabels('label', 'label1' + [j + 1], classLi1[0]);
+}
+var j;
+for (j = 0; j < 3; j + 1) {
+    obj.createLabels('label', 'label2' + [j + 1], classLi2[0]);
+}
+var j;
+for (j = 0; j < 3; j + 1) {
+    obj.createLabels('label', 'label3' + [j + 1], classLi3[0]);
+}
+
+
+//add answers
+
+elLabel = document.getElementsByTagName('label');
+
+//create checkbox
+
+var m;
+for (m = 0; m < 3; m + 1) {
+    obj.createText(elLabel[m], answerArr[m], 1);
+}
+var m;
+for (m = 3; m < 6; m + 1) {
+    obj.createText(elLabel[m], answerArr[m - 3], 1);
+}
+var m;
+for (m = 6; m < 9; m + 1) {
+    obj.createText(elLabel[m], answerArr[m -  6], 1);
+}
+//create checkbox
+var k;
+for (k = 0; k < elLabel.length; k) {
+    obj.createInputs('input', 'input', elLabel[k], 'checkbox', 0);
+    obj.createLabels('br', 'br', elLabel[k]);
+}
+
+//create submit
+var formTag = document.getElementsByTagName('form');
+obj.createInputs('input', 'submit', formTag[0], 'submit');
+
+
+
