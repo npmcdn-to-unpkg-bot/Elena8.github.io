606aa3c2afe75aed97db142ab454dbcd368b9b48
Elena8 <mol4anova_@mail.ru>
2016-06-01 23:40:59 +0300

    add ekzamen

---
+++ ekzamen/js/script.js
@@ -0,0 +1,49 @@
+$(function() {
+    'use strict';
+    function callback() {
+        var query = $('#query').val();
+        $.ajax({
+            url: "https://pixabay.com/api/",
+            data: {
+                key: "2630817-b95f50d1e0fc2b68ed1f4d734",
+                q: query
+            },
+            success: function (data) {
+                console.log(data);
+                var html,
+                    content;
+                    console.log(data.hits.length);
+                if(data.hits.length>0) {
+                    
+                    $(".grid").remove();
+                }
+                $('.content3__images').css('display','none');
+                html = $('#tmpl').html();
+                content = tmpl(html, data);
+                $('.grid__images').append(content);
+          
+            }
+        });
+    };
+    $('.content3__search').on('click', callback);
+
+    $('#query').keypress(function (e) {
+        if (e.which === 13) {
+            callback();
+        }
+    });
+
+    
+    $('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-item'
+});
+    
+    $('.flexslider').flexslider({
+    animation: "slide"
+  });
+    
+});
+
+
